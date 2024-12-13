f235b5dbc39bcf31bdb3e9b3b71c0dabc1364bca
Alex <delete.rap.stupid@gmail.com>
2024-12-13 13:22:29 +0100

    just for run tests again #2

--- cinema/views.py
+++ cinema/views.py
@@ -104,10 +104,7 @@ class MovieViewSet(
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get_serializer_class(self):
         if self.action == "list":
